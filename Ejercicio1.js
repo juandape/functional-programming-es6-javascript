74f1a992a1cd1dc593a75a4ae97c914f21b11a78
Juan David Peña <juandavidp76@hotmail.com>
2023-04-21 18:06:26 -0500

    ejercicios

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5, 6];
+
+  // utilizar el método map de los arreglos para duplicar cada elemento
+  // [2, 4, 6, 8, 10, 12]
+
+  // utilizar el método filter para filtrar los números pares de arr
+  // [2, 4, 6]
+
+  // utilizar el método reduce para sumar todos los elementos
+  // 21
